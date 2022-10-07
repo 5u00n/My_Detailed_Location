5ccdd8e754bd361accb7991b68c472a8c9a9e9e8
su00n <he.su00n@gmail.com>
2022-10-07 10:15:09 +0530

    second data

--- app/src/main/java/com/su00n/mydetailedlocation/WifiScanReceiver.java
+++ app/src/main/java/com/su00n/mydetailedlocation/WifiScanReceiver.java
@@ -121,7 +121,7 @@ public class WifiScanReceiver extends BroadcastReceiver {
 
                 for(DataSnapshot ds : dataSnapshot.getChildren()) {
                     WifiDistanceData wf=  ds.getValue(WifiDistanceData.class);
-                    Log.d(" Key Data : [ "+ds.getKey()+" ]", String.valueOf(wf.getDistance()));
+                    Log.d(" Key Data : [ "+ds.getKey()+" ", String.valueOf(wf.getDistance()));
                 }
 
 
